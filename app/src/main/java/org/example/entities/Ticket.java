fbb2806200e386355b68416c9758ff495b94f679
Anwesha Pramanik <bwumca23043@brainwareuniversity.ac.in>
2025-07-28 11:38:29 +0530

    Getter setters implemented

--- app/src/main/java/org/example/entities/Ticket.java
+++ app/src/main/java/org/example/entities/Ticket.java
@@ -1,12 +1,71 @@
 package org.example.entities;
 
-import java.util.Date;
+
+
+
+
+
 
 public class Ticket {
     private String ticketId;
     private String userId;
     private String source;
     private String destination;
-    private Date dateOfTravel;
+    private String dateOfTravel;
     private Train train;
+
+    public Ticket(){};
+    public Ticket(String ticketId, String userId, String source, String destination,
+                  String dateOfTravel, Train train){
+        this.ticketId=ticketId;
+        this.userId=userId;
+        this.source=source;
+        this.destination=destination;
+        this.dateOfTravel=dateOfTravel;
+        this.train=train;
+    }
+
+    private String getTicketId(){
+        return ticketId;
+    }
+
+    private String getUserId(){
+        return userId;
+    }
+    private String getSource(){
+        return source;
+    }
+    private String getDestination(){
+        return destination;
+    }
+    private String getDateOfTravel(){
+        return dateOfTravel;
+    }
+    private Train getTrain(){
+        return train;
+    }
+
+    private void setTicketId(String ticketId){
+        this.ticketId=ticketId;
+    }
+    private void setUserId(String userId){
+        this.userId=userId;
+    }
+    private void setDestination(String destination){
+        this.destination=destination;
+    }
+    private void setSource(String source){
+        this.source=source;
+    }
+    private void setDateOfTravel(String dateOfTravel){
+        this.dateOfTravel=dateOfTravel;
+    }
+    private void setTrain(Train train){
+        this.train=train;
+    }
+
+    public String getTicketInfo(){
+        return String.format("Ticket Id:%s belongs to User %s from %s to %s on %s",ticketId,userId,source,destination);
+    }
+
 }
--- app/src/main/java/org/example/entities/User.java
+++ app/src/main/java/org/example/entities/User.java
@@ -9,4 +9,47 @@ public class User {
     private List<Ticket> ticketsBooked;
     private String userId;
 
+
+    public User(String name, String password, String hashedPassword, List<Ticket> ticketsBooked,
+                String userId){
+        this.name=name;
+        this.password=password;
+        this.hashedPassword=hashedPassword;
+        this.ticketsBooked=ticketsBooked;
+        this.userId=userId;
+
+    }
+    public User(){
+        //if nothing is passed, nothing will happen
+    }
+
+    public String getName(){
+        return name;
+    }
+    public String getPassword(){
+        return password;
+    }
+    public String getHashedPassword(){
+        return hashedPassword;
+    }
+
+    public List<Ticket> getTicketsBooked() {
+        return ticketsBooked;
+    }
+
+    public void printTickets(){
+        for(int i=0;i<ticketsBooked.size();i++){
+            System.out.println(ticketsBooked.get(i).getTicketInfo());
+        }
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setName(String name){this.name=name;}
+    public void setPassword(String password){this.password=password;}
+    public void setHashedPassword(String hashedPassword){this.hashedPassword=hashedPassword;}
+    public void setTicketsBooked(List<Ticket> ticketsBooked){this.ticketsBooked=ticketsBooked;}
+    public void setUserId(String userId) {this.userId = userId;}
 }
